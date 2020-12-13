0c5819c47108196da56e893566c210c47df73968
Rudra Gahlot <72944412+RudraGahlot@users.noreply.github.com>
2020-12-13 18:15:48 +0530

    Create Variable.c

---
+++ C Programs/Variable.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    int inum=10;
+    float fnum= 10.3;
+    double dnum= 12;
+    long lnum= 39.3;
+    char cname='A';
+    char sname [10]="RUDRA";
+    printf("int =%d,float=%f,double=%lf,long=%ld,char=%c,String=%s",inum,fnum,dnum,lnum,cname,sname);
+     return 0;
+}
