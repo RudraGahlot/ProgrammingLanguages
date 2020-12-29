a187562f1e2ee189443f77ad40cbb570b45caa52
Rudra Gahlot <72944412+RudraGahlot@users.noreply.github.com>
2020-12-29 14:32:19 +0530

    Create typeCasting.java

---
+++ JAVA/typeCasting.java
@@ -0,0 +1,21 @@
+package typeCasting;
+
+public class TypeCasting {
+
+	public static void main(String[] args) {
+		
+	//	int x=4;
+	//	double y=x;
+		//System.out.println(x);
+	//	System.out.println(y);
+
+		
+		double myDouble = 3.14;
+		int myInt = (int)myDouble;
+		
+		System.out.println(myDouble);
+		
+		System.out.println(" integer value is " + myInt);
+	}
+
+}
