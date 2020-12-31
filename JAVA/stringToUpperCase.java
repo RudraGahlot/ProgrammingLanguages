7851432926cc5785a835d68f1d50a5da9e295c57
Rudra Gahlot <72944412+RudraGahlot@users.noreply.github.com>
2020-12-31 14:35:53 +0530

    Create stringToUpperCase.java

---
+++ JAVA/stringToUpperCase.java
@@ -0,0 +1,17 @@
+package stringIntoUpperCase;
+
+
+public class StringIntoUpperCase {
+
+	public static void main(String[] args) {
+		
+		String str = "my name is rudra gahlot";
+		String strUpper = str.toUpperCase();
+		
+		System.out.println("Orignal String :" + str);
+		
+		System.out.println("String Change To Uppercase : " + strUpper);
+		
+	}
+
+}
