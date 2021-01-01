af78d0ce253e81a910da5ed39ae70123587875b1
Rudra Gahlot <72944412+RudraGahlot@users.noreply.github.com>
2021-01-01 14:43:49 +0530

    Create arithmeticOperator.java

---
+++ JAVA/Operators/arithmeticOperator.java
@@ -0,0 +1,19 @@
+package arithematicOperator;
+
+public class ArithmeticOperator {
+
+	public static void main(String[] args) {
+		
+		int a = 3;
+		int b = 21;
+		 
+//		int c = b/a;
+		
+//		int c = b++;
+		
+		int c = --b;
+		System.out.println(c + " " + b);
+
+	}
+
+}
