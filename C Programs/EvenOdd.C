baf7537165549163f0e4d40c85d747d0c670448a
Rudra Gahlot <72944412+RudraGahlot@users.noreply.github.com>
2021-01-04 15:33:30 +0530

    Create EvenOdd.C

---
+++ C Programs/EvenOdd.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int num;
+printf( "Enter a number :"); 
+scanf("%d",&num);
+if (num % 2  == 0)
+printf(" Even Number ");
+ 
+else
+printf(" Odd Number ");
+     return 0;
+}
