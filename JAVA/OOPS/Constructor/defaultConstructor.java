db2b8cbb7be9b4e7737b1dd8ad65ba48d0e02129
Rudra Gahlot <72944412+RudraGahlot@users.noreply.github.com>
2021-05-05 20:05:27 +0530

    Create defaultConstructor.java

---
+++ JAVA/OOPS/Constructor/defaultConstructor.java
@@ -0,0 +1,18 @@
+package constructorsInJava;
+
+public class defaultConstructor {
+	
+
+		defaultConstructor()
+		{
+			System.out.println(" Bike is created");
+		}
+	
+
+	
+	public static void main(String[] args) 
+	
+	{
+				defaultConstructor b = new defaultConstructor();
+	}
+}
